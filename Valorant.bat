491faeaf1e4d67fdd942e2b912c062e8130dae28
LEMON シ <87497636+TheFrenchRageur@users.noreply.github.com>
2022-08-25 02:07:52 +0200

    Add files via upload

---
+++ Valorant.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set valPath="(add your valorant path here)\Riot Games\VALORANT\live\ShooterGame\Content\Movies\Menu\HomeScreen.mp4"
+set videoPath="%CD%\HomeScreen.mp4"
+REM set videoPath="(remove 'REM' and replace this by your custom video path)"
+
+%USERPROFILE%\Desktop\VALORANT.url
+del %valPath%
+xcopy %videoPath% %valPath%*
+
+mshta "about:<script>alert('Press "Ok" when you see Play on riot client, then wait.');close()</script>"
+
+del %valPath%
+xcopy %videoPath% %valPath%*
+%USERPROFILE%\Desktop\VALORANT.url
