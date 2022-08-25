47ff22e5143b2ac37a68c8e431c2757a7f61d731
LEMON シ <87497636+TheFrenchRageur@users.noreply.github.com>
2022-08-25 02:41:15 +0200

    Update Valorant.bat

--- Valorant.bat
+++ Valorant.bat
@@ -2,7 +2,7 @@
 
 set valPath="(add your valorant path here)\Riot Games\VALORANT\live\ShooterGame\Content\Movies\Menu\HomeScreen.mp4"
 set videoPath="%CD%\HomeScreen.mp4"
-REM set videoPath="(remove 'REM' and replace this by your custom video path)"
+REM set videoPath="(remove 'REM' and replace this by your custom video path)\HomeScreen.mp4"
 
 %USERPROFILE%\Desktop\VALORANT.url
 del %valPath%
